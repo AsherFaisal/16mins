124cbe88211061fbcc3def8a76463848dd6c7435
asher <15080625+AsherFaisal@users.noreply.github.com>
2025-06-24 00:07:22 -0400

    added github to serve static content

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/16mins/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -27,7 +28,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "../docs"),
     emptyOutDir: true,
   },
   server: {
